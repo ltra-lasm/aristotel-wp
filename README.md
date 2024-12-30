0ebd29acd4f57f970fcdd7e84e91e3fc6a3f0e1f
Daniel Dubrovschi <danihere@rells.xyz>
2024-12-30 04:11:13 +0200

    new file:   .gitignore 	new file:   README.md 	new file:   app.py 	new file:   static/css/styles.css 	new file:   templates/achievements.html 	new file:   templates/base.html 	new file:   templates/contact.html 	new file:   templates/hobbies.html 	new file:   templates/index.html

---
+++ README.md
@@ -0,0 +1,4 @@
+# Portfolio website, but better
+
+
+A Changed version of my portfolio website, to be lighter, faster and even more fun!
---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/achievements')
+def achievements():
+    return render_template('achievements.html')
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        # Add functionality to send the message (e.g., via email)
+        return render_template('contact.html', success=True)
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=False)
